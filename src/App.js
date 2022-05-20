d3a2afeca33113e4861566ed44976eedad3187ce
Yinan Li <gabriel.li5566@gmail.com>
2022-05-20 20:26:33 +0300

    config routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
+import Home from './pages/Layout'
+import Login from './pages/Login'
+import NotFound from './pages/404'
 function App() {
-  return <div className="App">App Root Component</div>
+  return (
+    <Router>
+      <div className="App">
+        <Link to="/login">Login</Link>
+        <Link to="/home">Home</Link>
+
+        {/* Routing */}
+        <Routes>
+          <Route index element={<Home />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
 export default App
