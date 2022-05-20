b8dd865109afd75b2d892cab8f78148df6846832
Yinan Li <gabriel.li5566@gmail.com>
2022-05-20 23:26:50 +0300

    login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
-import { Button, DatePicker } from 'antd'
+import { Card } from 'antd'
+import './index.css'
+import logo from '../../assets/logo.png'
 
 export default class Login extends Component {
   render() {
     return (
-      <div>
-        Login Component<Button type="primary">Button</Button> <DatePicker />
+      <div className="login">
+        <Card className="login-container">
+          <img src={logo} alt="" className="login-logo" />
+        </Card>
       </div>
     )
   }
