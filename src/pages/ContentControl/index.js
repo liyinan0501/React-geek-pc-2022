80c6c6b1d8b4dcfa27723a0d51d7b77c8db8b033
Yinan Li <gabriel.li5566@gmail.com>
2022-06-25 23:08:22 +0300

    init content control component

--- src/pages/ContentControl/index.js
+++ src/pages/ContentControl/index.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react'
+import styles from './index.module.scss'
+import { Card, Breadcrumb } from 'antd'
+import { Link } from 'react-router-dom'
 
 export default class ContentControl extends Component {
   render() {
-    return <div>ContentControl</div>
+    return (
+      <div className={styles.root}>
+        <Card
+          title={
+            <Breadcrumb>
+              <Breadcrumb.Item>
+              <Link to= '/home'>Home</Link></Breadcrumb.Item>
+              <Breadcrumb.Item>
+                <a href="">Content Control</a>
+              </Breadcrumb.Item>
+            </Breadcrumb>
+          }
+        ></Card>
+      </div>
+    )
   }
 }
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import styles from './index.module.scss'
+
 export default class Dashboard extends Component {
   render() {
     return <div className={styles.dashboard}></div>
