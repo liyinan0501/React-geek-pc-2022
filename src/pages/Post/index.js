1d8960f5d43dadde1174072476a18d27b8270a8c
Yinan Li <gabriel.li5566@gmail.com>
2022-06-27 00:50:22 +0300

    Editing upload in post page

--- src/pages/Post/index.js
+++ src/pages/Post/index.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react'
-import { Card, Breadcrumb, Form, Button, Space, Input } from 'antd'
+import {
+  Card,
+  Breadcrumb,
+  Form,
+  Button,
+  Space,
+  Input,
+  Radio,
+  Upload,
+} from 'antd'
 import { Link } from 'react-router-dom'
 import Channel from 'components/Channel'
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
+import styles from './index.module.scss'
+import { PlusOutlined } from '@ant-design/icons'
 
 export default class Post extends Component {
   state = {
-    channels: [],
+    type: 1,
   }
   render() {
     return (
-      <div>
+      <div className={styles.root}>
         <Card
           title={
             <Breadcrumb>
@@ -25,7 +38,7 @@ export default class Post extends Component {
             size="large"
             onFinish={this.onFinish}
             validateTrigger={['onBlur', 'onChange']}
-            initialValues={{}}
+            initialValues={{ content: '', type: this.state.type }}
           >
             <Form.Item
               label="Title"
@@ -54,8 +67,35 @@ export default class Post extends Component {
             >
               <Channel></Channel>
             </Form.Item>
-            <Form.Item label="Cover"></Form.Item>
-            <Form.Item label="Content"></Form.Item>
+            <Form.Item label="Cover" name="type">
+              <Radio.Group onChange={this.changeType}>
+                <Radio value={1}>Single photo</Radio>
+                <Radio value={3}>Triple photos</Radio>
+                <Radio value={0}>No photos</Radio>
+              </Radio.Group>
+            </Form.Item>
+            <Form.Item wrapperCol={{ offset: 4 }}>
+              {this.state.type !== 0 && (
+                <Upload listType="picture-card">
+                  <PlusOutlined></PlusOutlined>
+                </Upload>
+              )}
+            </Form.Item>
+            <Form.Item
+              label="Content"
+              name="content"
+              rules={[
+                {
+                  required: true,
+                  message: 'The content can not be empty.',
+                },
+              ]}
+            >
+              <ReactQuill
+                theme="snow"
+                placeholder="Input the content of article"
+              />
+            </Form.Item>
             {/* 距离左边偏移4 */}
             <Form.Item wrapperCol={{ offset: 4 }}>
               <Space>
@@ -69,6 +109,11 @@ export default class Post extends Component {
         </Card>
       </div>
     )
+  }
+  changeType = (e) => {
+    this.setState({
+      type: e.target.value,
+    })
   }
   onFinish = (values) => {
     console.log(values)
