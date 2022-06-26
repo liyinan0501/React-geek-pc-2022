6e6aaf6f485e46a5b1b907e8b6cd2c3a76f3a58f
Yinan Li <gabriel.li5566@gmail.com>
2022-06-26 16:13:55 +0300

    get Article list

--- src/pages/ContentControl/index.js
+++ src/pages/ContentControl/index.js
@@ -1,16 +1,88 @@
 import React, { Component } from 'react'
 import styles from './index.module.scss'
-import { Card, Breadcrumb, Form, Radio, Button, Select } from 'antd'
+import {
+  Card,
+  Breadcrumb,
+  Form,
+  Radio,
+  Button,
+  Select,
+  DatePicker,
+  Table,
+} from 'antd'
 import { Link } from 'react-router-dom'
 import { ArticleStatus } from 'api/constants'
 import { getChannels } from 'api/channel'
+import { getArticles } from 'api/article'
 
 const { Option } = Select
+const { RangePicker } = DatePicker
 
 export default class ContentControl extends Component {
+  columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+    },
+    {
+      title: 'Title',
+      dataIndex: 'title',
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+    },
+    {
+      title: 'Publish date',
+      dataIndex: 'tags',
+    },
+    {
+      title: 'Views',
+      dataIndex: 'tags',
+    },
+    {
+      title: 'Comments',
+      dataIndex: 'tags',
+    },
+    {
+      title: 'Likes',
+      dataIndex: 'tags',
+    },
+    {
+      title: 'Action',
+      dataIndex: 'tags',
+    },
+  ]
+
+  data = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+      tags: ['nice', 'developer'],
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+      tags: ['loser'],
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+  ]
+
   state = {
     channels: [],
+    articles: {},
   }
+
   render() {
     return (
       <div className={styles.root}>
@@ -47,6 +119,10 @@ export default class ContentControl extends Component {
               </Select>
             </Form.Item>
 
+            <Form.Item label="Date" name="date">
+              <RangePicker />
+            </Form.Item>
+
             <Form.Item>
               <Button type="primary" htmlType="submit">
                 Filter
@@ -54,15 +130,29 @@ export default class ContentControl extends Component {
             </Form.Item>
           </Form>
         </Card>
+        <Card title={`Total xxx results:`}>
+          <Table columns={this.columns} dataSource={this.data} />
+        </Card>
       </div>
     )
   }
 
   async componentDidMount() {
-    const res = await getChannels()
+    this.getChannelList()
+    this.getArticleList()
+  }
 
+  async getChannelList() {
+    const res = await getChannels()
     this.setState({
       channels: res.data.channels,
+    })
+  }
+
+  async getArticleList() {
+    const res = await getArticles()
+    this.setState({
+      articles: res.data,
     })
   }
 
